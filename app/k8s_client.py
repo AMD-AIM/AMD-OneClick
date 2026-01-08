ef0b750cc842423016fdb3765d5118517b0cf3b7
vivienfanghua <vivienfanghua@163.com>
2026-01-08 11:27:59 +0800

    Remove nodeName binding and update resource config to 8 CPU/32Gi

--- app/k8s_client.py
+++ app/k8s_client.py
@@ -108,7 +108,6 @@ jupyter lab --ip=0.0.0.0 --port={settings.NOTEBOOK_PORT} --no-browser --allow-ro
                 "annotations": annotations
             },
             "spec": {
-                "nodeName": "wx-ms-w7900d-0004",
                 "dnsPolicy": "None",
                 "dnsConfig": {
                     "nameservers": ["1.1.1.1", "8.8.8.8", "8.8.4.4"],
